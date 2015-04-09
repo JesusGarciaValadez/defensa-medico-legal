7d44d51432f8106aecf7a95296bc0c29d965405e
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-04-09 21:09:52 +0000

    home markup fir round done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,48 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends( 'app' )
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+@section( 'title', 'Defensa Médico Legal' )
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+@section( 'section-class', 'home' )
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
-</html>
+@section( 'content' )
+            <section class="central clearfix content">
+                <img src="" alt="">
+                <section class="clearfix advance">
+                    {!! Html::image( '', 'Con determinación buscamos la Justicia' ) !!}
+                    <p>!Te quieren obligar a pagar año con año por ejercer tu profesión de por vida!</p>
+                    <p>PROTEGETE CON NOSOTROS</p>
+                </section>
+                <section class="clearfix advance">
+                    {!! Html::image( '', 'Con determinación buscamos la Justicia' ) !!}
+                    <p>Las instancias más altas nos respaldan. La Ley es igual para todos y la justicia no tiene miramientos.</p>
+                </section>
+                <section class="clearfix advance">
+                    {!! Html::image( '', 'Con determinación buscamos la Justicia' ) !!}
+                    <p>Conoce los derechos y regulaciones que la comisión Interamericana ha desarrollado en pro de nosotros.</p>
+                </section>
+                <aside class="cleafix">
+                    <p>AIDDEM busca proteger todos los intereses de los médicos de manera individual o de manera colectiva.</p>
+                    <p>Individualmente  podemos conseguirte una suspensión de esta ley para que todo este tiempo que dura el amparo, tú no seas molestado y que cuando ganemos este amparo, definitivamente tú serás de los médicos que gocen que la ley no sea aplicada para ti.</p>
+                </aside>
+                <section class="clearfix sign">
+                    <p>Último día 13 de abril para realiza tu amparo individual. <a href="" title="!regístrate ahora!" target="_blank">!regístrate ahora!</a></p>
+                </section>
+                <section class="clearfix affiliation">
+                    <aside>
+                        <figure>
+                            {!! Html::image( '', 'Con determinación buscamos la Justicia' ) !!}
+                            <figcaption><p>Afíliate</p></figcaption>
+                        </figure>
+                        <p>No permitas que se violenten tus derechos</p>
+                    </aside>
+                    <section class="commission">
+                        <figure>
+                            <a href="" title="" target="_blank">
+                                {!! Html::image( '', 'Con determinación buscamos la Justicia' ) !!}
+                            </a>
+                            <figcaption>La Corte Interamericana de Derechos Humanos es un órgano judicial de la Organización de los Estados Americanos (OEA) que goza de autonomía frente a los demás órganos de aquella y que tiene su sede en San José de Costa Rica, cuyo propósito es aplicar e interpretar la Convención Americana sobre Derechos Humanos y otros tratados de derechos humanos a los cuales se somete el llamado sistema interamericano de protección de derechos humanos.</figcaption>
+                        </figure>
+                    </section>
+                </section>
+            </section>
+@stop
