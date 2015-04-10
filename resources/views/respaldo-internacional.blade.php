872c4a1a80251da7270dd92f5360b39d8ad5902d
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-04-10 08:26:50 +0000

    Respaldo internacional markup, image and style done

---
+++ resources/views/respaldo-internacional.blade.php
@@ -0,0 +1,57 @@
+@extends( 'app' )
+
+@section( 'title', 'Beneficios - Respaldo Internacional' )
+
+@section( 'section-class', 'respaldo' )
+
+@section( 'content' )
+            <section class="internal clearfix content bit-1">
+                <article class="clearfix bit-1">
+                    {!! Html::image( 'img/respaldo-internacional/comision-interamericana-de-derechos-humanos.jpg', 'Comision Interamericana de Derechos Humanos', [ 'class' => 'bit-1' ] ) !!}
+                    <h1 class="bit-1">ORGANIZACIONES INTERNACIONALES EN MATERIA DE DERECHOS HUMANOS.</h1>
+                    <ul class="bit-1">
+                        <li>Corte Interamericana de Derechos Humanos.</li>
+                        <li>Comisión Interamericana de Derechos Humanos.</li>
+                        <li>Libertad de Ejercicio Profesional.</li>
+                        <li>Derecho Humano al Avance Científico.</li>
+                        <li>Principio de Progresividad de los Derechos Humanos.</li>
+                        <li>Criterios de Índole Internacional.</li>
+                    </ul>
+                    <h2 class="bit-1">COMISIÓN INTERAMERICANA DE DERECHOS HUMANOS.</h2>
+                    <p class="bit">La comisión, por un lado, tiene competencias con dimensiones políticas, entre cuyas tareas destacan la realización de visitas “in loco” y la preparación de informes con sus observaciones acerca de la situación de derechos humanos en los estados miembros.</p>
+                    <p class="bit-1">Por otro lado, la CIDH realiza funciones con una dimensión “cuasi-judicial”. Es dentro de esta competencia de la CIDH que recibe las denuncias de particulares u organizaciones relativas a violaciones a derechos humanos, examina esas peticiones y adjudica los casos en el supuesto de que se cumplan los requisitos de admisibilidad.</p>
+                    <p class="bit-1"><strong>PROCEDIMIENTO:</strong></p>
+                    <ol class="bit-1">
+                        <li>Se presenta la petición y se analiza que cumpla con los requisitos formales.</li>
+                        <li>Se transmite al estado denunciado para que presente observaciones.</li>
+                        <li>Se ponen a disposición las partes interesadas para llegar a la solución amistosa.</li>
+                        <li>De no obtener resultado favorable, se remite el caso a la “Corte Interamericana de Derechos Humanos”.</li>
+                    </ol>
+                    <h2 class="bit-1">CORTE INTERAMERICANA DE DERECHOS HUMANOS.</h2>
+                    <p class="bit-1">Su función se centra en salvaguardar los derechos esenciales del hombre en el continente americano, a través de los órganos competentes para conocer de las violaciones a los derechos humanos, trabajando en colaboración con la “Comisión Interamericana de Derechos Humanos”.</p>
+                    <p class="bit-1">La Corte tiene esencialmente dos funciones, una función contenciosa y otra función consultiva, a las que se une la facultad de adoptar medidas provisionales.</p>
+                    <p class="bit-1">En cuanto a la función contenciosa, se trata del mecanismo por el cuál la corte, determina si un estado ha incurrido en responsabilidad internacional por haber violado alguno de los derechos consagrados o estipulados en la “Convención Americana sobre Derechos Humanos”.</p>
+                    <p class="bit-1">En cuanto a la función consultiva, es el medio por el cual la corte responde consultas que formulan los estados miembros de la OEA o los órganos de la misma. Esta competencia consultiva fortalece la capacidad de la organización para resolver los asuntos que surjan por la aplicación de la convención, ya que permite a los órganos de la OEA consultar a la corte en lo que les compete.</p>
+                    <h3 class="bit-1">¿CUÁLES SON LOS DERECHOS HUMANOS PROTEGIDOS?</h3>
+                    <p class="bit-1">La Comisión es competente para examinar peticiones en las que se aleguen violaciones a los derechos humanos contenidos en la Declaración Americana, la Convención Americana y otros tratados interamericanos de derechos humanos.</p>
+                    <p class="bit-1">En la búsqueda por una salud integral:</p>
+                    <ul class="bit-1">
+                        <li>Derecho a la integridad de las personas, se ve afectado al negarse a la práctica de la  Cirugía Estética por profesionales de la salud, debidamente preparados para ejercer esta rama de la Medicina.</li>
+                        <li>El negar el ejercicio profesional, en específico a los Cirujanos con Maestría Estética, constituye una violación al proceso  e innovación tecnológica, así como el derecho de la integridad del usuario o paciente.</li>
+                    </ul>
+                    <h3 class="bit-1">DERECHO A LA LIBERTAD DE EJERCICIO PROFESIONAL</h3>
+                    <p class="bit-1"><strong>DECLARACIÓN UNIVERSAL DE DERECHOS HUMANOS:</strong></p>
+                    <p class="bit-1"><strong>Artículo 23:</strong> Toda persona tiene derecho al trabajo, a la libre elección de su trabajo, a condiciones equitativas y satisfactorias de trabajo y a la protección contra el desempleo.</p>
+                    <h3 class="bit-1">DERECHO HUMANO AL AVANCE CIENTIFICO.</h3>
+                    <p class="bit-1">El derecho a la salud no puede disociarse de los avances científicos que permiten a las personas un mayor bienestar y una mejor calidad de vida, tanto en la promoción de la salud como en la prevención y la atención de la enfermedad o las dolencias.</p>
+                    <h3 class="bit-1">PRINCIPIO DE PROGRESIVIDAD DE LOS DERECHOS HUMANOS. AVANCE CIENTÍFICO</h3>
+                    <p class="bit-1">Una última generación de derechos relacionados con los más recientes avances científicos y tecnológicos, son considerados “derechos no escritos” en la mayor parte de los sistemas europeos (aunque paradójicamente la Carta de los Derechos Fundamentales de la Unión Europea es pionera en este ámbito), dado que sólo en los textos del final del siglo XX e inicios del presente empiezan a encontrar reflejo escrito.</p>
+                    <p class="bit-1">Son aquellos vinculados con la identidad genética, la integridad genética y la intimidad genética, el con sentimiento informado en todas las intervenciones relacionadas con la salud, la protección de datos personales, el acceso universal a las nuevas tecnologías, la “intimidad informática”, entre otros.</p>
+                    <h3 class="bit-1">CRITERIOS DE ÍNDOLE INTERNACIONAL.</h3>
+                    <p class="bit-1"><strong>El derecho a disfrutar de los beneficios del avance científico</strong></p>
+                    <ul class="bit-1">
+                        <li>Declaración de Viena (1993). Párrafo 11. Todos tienen derecho a disfrutar del progreso científico y de sus aplicaciones.</li>
+                        <li>Plataforma de Acción de Beijing (1995) .Párrafo 109 (h). Los gobiernos deberán prestar apoyo financiero e institucional a la investigación sobre métodos y tecnologías seguros, eficaces, asequibles y aceptables para la salud reproductiva y sexual de las mujeres y los hombres, incluidos métodos más seguros, eficaces, asequibles y aceptables para la regulación de la fecundidad incluida la planificación natural de la familia para ambos sexos, métodos para la protección contra el VIH/SIDA y otras enfermedades de transmisión sexual y métodos sencillos y baratos para el diagnóstico de tales enfermedades, entre otras cosas. Estas investigaciones deben guiarse en todas las etapas por los usuarios y han de llevarse a cabo desde la perspectiva de la distinta condición entre varones y mujeres, en particular desde la perspectiva de género, y realizarse en estricta conformidad con normas de investigación biomédica, jurídicas, éticas, médicas y científicas internacionalmente aceptadas.</li>
+                    </ul>
+            </section>
+@stop
