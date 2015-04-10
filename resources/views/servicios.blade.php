5da85751119a9bf20ae7c5aa16534e6ad44f33b5
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-04-10 21:14:04 +0000

    New fixes in text in servicios section

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -14,7 +14,7 @@
                     <p class="bit-1">Amparo en el cual se busca la declaración judicial de inconstitucionalidad de la norma general que se quiere aplicar en tu perjuicio, solicitando previamente la  suspensión con la cual se evitará que de inmediato se aplique la norma en tu perjuicio hasta en tanto se resuelva en definitiva el amparo.</p>
                     <p class="bit-1">Toma en cuenta que las resoluciones finales son dictadas por un Juez o un Tribunal.</p>
                     <p class="bit-1">Este amparo tiene un costo, pero te da la tranquilidad de que estás protegido todo este tiempo, el costo sería:</p>
-                    <p class="bit-1">Si tu trajeras un médico invitado, nada más pagarías el 50% o sea $15 mil pesos, nosotros te reembolsaríamos la diferencia. Si tu trajeras un segundo medico el costo sería de $9 mil pesos, si traes un tercer médico no tiene costo tu amparo en absoluto. Pero para poder iniciar tus defensa legal de manera individual, necesitamos que haga el deposito del 50% de manera individual que son $15 mil pesos, si ingresan más médicos se haría la devolución correspondiente. Contratando el amparo individual, tienes el amparo colectivo de manera completamente gratuita.</p>
+                    <p class="bit-1">el costo del amparo es de $22,500.00 pagando $13,000.00 al momento de contratar y los restantes $9,500.00 al final, los descuentos por médicos que traigas y contraten el amparo son del 50% por el 1° médico, con el  2° pagarás solo $7,000.00 y con el tercero tu amparo es gratis.</p>
                     <p class="bit-1"><strong>Nosotros corremos con todos los gastos de tramitación judicial.</strong></p>
                     <p class="bit-1">Si tu decisión es contratar nuestros servicios para el amparo individual lo que tienes que hacer es lo siguiente:</p>
                     <ol class="bit-1">
@@ -22,7 +22,7 @@
                         <li>Aceptar el contrato</li>
                         <li>Depositos a la cuenta número: 4041280041, clabe: 021180040412800414 de HSBC, a nombre de Arroyo Abogados S.C.</li>
                         <li>El despacho Arroyo Abogados S.C., al cual realizas el pago es el principal afiliado del Consejo Mexicano de la Defensa Médico Legal y fundamental promotor de la defensa jurídica de tus Derechos.</li>
-                        <li>Mandar ficha de pago a <a href="mailto:depositos@aiddem.org" target="_blank" title="depositos@aiddem.org">depositos@aiddem.org</a></li>
+                        <li>Mandar ficha de pago a <a href="mailto:contactodefensamedicolegal@gmail.com" target="_blank" title="contactodefensamedicolegal@gmail.com">contactodefensamedicolegal@gmail.com</a></li>
                         <li>Nuestros abogados se pondrán en contacto con usted para pedirle entre otros documentos lo siguiente:
                             <ul class="bit-1 clearfix">
                                 <li>5.1.- Carta poder</li>
