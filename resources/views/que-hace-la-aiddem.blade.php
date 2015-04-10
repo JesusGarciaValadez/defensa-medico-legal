b0a778a7e98ad1a93b25bb98ec9f28a54552b750
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-04-10 07:19:58 +0000

    Que hace la aiddemm markup, image and styles done

---
+++ resources/views/que-hace-la-aiddem.blade.php
@@ -0,0 +1,26 @@
+@extends( 'app' )
+
+@section( 'title', 'AIDDEM - ¿Qué hace la AIDDEM?' )
+
+@section( 'section-class', 'que-hace-la-aiddem' )
+
+@section( 'content' )
+            <section class="internal clearfix content bit-1">
+                <article class="clearfix bit-1">
+                    <h1 class="bit-1">¿QUE HACE LA AIDDEM?</h1>
+                    <article class="bit-3 clearfix">
+                        {!! Html::image( 'img/que-hace-la-aiddem/manos-entrelazadas.jpg', 'Manos entrelazadas', [ 'class' => 'bit-1' ] ) !!}
+                    </article>
+                    <article class="bit-666 clearfix">
+                        <h2 class="bit-1">¡LA AIDDEM DEFIENDE TUS DERECHOS!</h2>
+                        <p class="bit-1">El gobierno no amenaza, ya viola nuestros derechos humanos, como lo es la libertad de profesión.</p>
+                        <p class="bit-1">No solo no crea fuentes de trabajo, si no que las cierra sin miramientos</p>
+                        <p class="bit-1">¿Qué hacer? ¿Cambiar de profesión?</p>
+                        <p class="bit-1">Somos los médicos cirujanos, con experiencia y preparación, quienes derecho a alzar la voz y decidir sobre la calidad de la salud ¡No los burócratas!</p>
+                        <p class="bit-1">Los charlatanes son quienes nos descalifican porque nosotros contamos con la experiencia y validación de las autoridades de salud.</p>
+                        <p class="bit-1">Únete a este movimiento gratuito que solo pretende mantener nuestras fuentes de empleo.</p>
+                        <p class="bit-1"><strong>"Nuestra defensa no es en México, si no ante los órganos internacionales, en particular la corte Interamericana de los Derechos Humanos."</strong></p>
+                        <p class="bit-1">La Corte Interamericana de Derechos Humanos es un órgano judicial de la Organización de los Estados Americanos (OEA) que goza de autonomía frente a los demás órganos de aquella y que tiene su sede en San José de Costa Rica, cuyo propósito es aplicar e interpretar la Convención Americana sobre Derechos Humanos y otros tratados de derechos humanos a los cuales se somete el llamado sistema interamericano de protección de derechos humanos.</p>
+                    </article>
+            </section>
+@stop
