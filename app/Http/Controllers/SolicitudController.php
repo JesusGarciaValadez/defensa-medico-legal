365af4b2b0c648d0750c96f05b22f50384b97553
alfabrad <zerovamp@hotmail.com>
2015-04-10 23:03:59 +0000

    Correct email address in email template

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -55,7 +55,7 @@ class SolicitudController extends Controller {
             $mail->setFrom( "contactodefensamedicolegal@gmail.com", "Defensa Medico Legal" );
             $mail->Subject = "Datos de tu solicitud";
             $mail->isHTML(true);// Set email format to HTML
-            $mail->Body    = '<h1>Recibimos tu solicitud ¡Muchas gracias!</h1><p>Recuerda, los datos de depósito son a la cuenta número: 4041280041, clabe: 021180040412800414 de HSBC, a nombre de Arroyo Abogados S.C.</p><p><b>Contáctanos</b></p><ul><li>Montecito 38, piso 8 oficina 29. Col. Napoles. World trade center.</li><li>03810 México D.F.</li><li>Teléfonos. 52944965, fax 55899147 ext. 11</li><li>Conmutador. 90002640 ext. 47</li><li>Correo electrónico. <a href="mailto:contacto@defensamedicolegal.com.mx" title="contacto@defensamedicolegal.com.mx" target="_blank">contacto@defensamedicolegal.com.mx</a></li>';
+            $mail->Body    = '<h1>Recibimos tu solicitud ¡Muchas gracias!</h1><p>Recuerda, los datos de depósito son a la cuenta número: 4041280041, clabe: 021180040412800414 de HSBC, a nombre de Arroyo Abogados S.C.</p><p><b>Contáctanos</b></p><ul><li>Montecito 38, piso 8 oficina 29. Col. Napoles. World trade center.</li><li>03810 México D.F.</li><li>Teléfonos. 52944965, fax 55899147 ext. 11</li><li>Conmutador. 90002640 ext. 47</li><li>Correo electrónico. <a href="mailto:contactodefensamedicolegal@gmail.com" title="contactodefensamedicolegal@gmail.com" target="_blank">contactodefensamedicolegal@gmail.com</a></li>';
             $mail->addAddress( $email, $doctor );
             $mail->send();
         }
