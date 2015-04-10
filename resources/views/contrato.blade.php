01ac6fa0ed4e8aa26b93a0a17a5275f1681fbd19
Jesus Antonio Garcia Valadez <zerovamp@hotmail.com>
2015-04-10 10:46:31 +0000

    Contrato markup and styles done

---
+++ resources/views/contrato.blade.php
@@ -0,0 +1,54 @@
+@extends( 'app' )
+
+@section( 'title', 'Contrato de promesa de prestación de servicios profesionales en materia legal.' )
+
+@section( 'section-class', 'contrato' )
+
+@section( 'content' )
+            <section class="internal clearfix content bit-1">
+                <article class="clearfix bit-1">
+                    <p class="bit-1">CONTRATO DE PROMESA DE PRESTACIÓN DE SERVICIOS PROFESIONALES EN MATERIA LEGAL, QUE CELEBRAN POR UNA PARTE <strong>EL CONSEJO MEXICANO DE LA DEFENSA MÉDICO LEGAL</strong>, REPRESENTADA POR EL MAESTRO EN AMPARO MIGUEL ARROYO RAMÍREZ, A QUIEN EN LO SUCESIVO Y PARA LOS EFECTOS DE ESTE CONTRATO SE LE DENOMINARA <strong>“EL PROFESIONISTA”</strong> Y POR LA OTRA, EL SEÑOR _______________________________, A QUIEN EN LO SUCESIVO Y PARA EFECTOS DEL PRESENTE INSTRUMENTO SE LE DENOMINARÁ <strong>“EL CLIENTE”</strong>, QUIENES SE SUJETAN AL TENOR DE LAS SIGUIENTES DECLARACIONES Y CLÁUSULAS:</p>
+                    <h1 class="bit-1">D  E  C  L  A  R  A  C  I  O  N  E  S</h1>
+                    <p class="bit-1">I.- DECLARA <strong>“EL PROFESIONISTA”</strong>, por conducto de su representante legal:</p>
+                    <ul class="bit-1">
+                        <li>Que cuenta con la capacidad, experiencia, personal calificado y las instalaciones necesarias para llevar a cabo la prestación de los servicios profesionales que requiere “EL CLIENTE”.</li>
+                        <li>Que tiene su domicilio en Calle Montecito No. 38 (Edificio World Trade Center), Piso 8, Oficina 29, Colonia Nápoles, México, Distrito Federal.</li>
+                        <li>Que su representante cuenta con las facultades necesarias para celebrar el presente contrato, las cuales no le han sido revocadas ni limitadas en forma alguna.</li>
+                    </ul>
+                    <p class="bit-1">II.- DECLARA “EL CLIENTE”:</p>
+                    <ul class="bit-1">
+                        <li>Ser  persona  física con residencia en los Estados Unidos Mexicanos, con la capacidad legal necesaria para el otorgamiento del presente contrato.</li>
+                        <li>Tener como profesión la de Médico ________________, con la especialidad en ____________________.</li>
+                        <li>Que tiene su domicilio ubicado en __________________________________________.</li>
+                        <li>Que requiere la asistencia y los servicios profesionales de <strong>“EL PROFESIONISTA”</strong>, para que le proporcione asesoría, representación jurídica y el patrocinio legal para la interposición y tramitación del Juicio de Amparo, en todas sus instancias, en contra de las disposiciones legales (normas generales, leyes, decretos, acuerdos, etc) y Autoridades que atentan contra sus intereses, garantías y derechos fundamentales relativos al libre ejercicio de su profesión. Así como para su representación y defensa ante instancias y Cortes Internacionales por la posible vulneración a esos derechos fundamentales por parte de cualquier Autoridad del Estado Mexicano.</li>
+                        <li>Que es su voluntad someterse a los términos y condiciones establecidos en el presente contrato, a efecto de llevar a cabo la celebración del contrato definitivo de Prestación de Servicios Profesionales.</li>
+                    </ul>
+                    <p class="bit-1">III.- DECLARAN <strong>AMBAS PARTES</strong>:</p>
+                    <ul class="bit-1">
+                        <li>Que es su voluntad celebrar el presente contrato y que en el mismo no existe lesión, dolo, mala fe o algún otro vicio que pudiera invalidarlo.</li>
+                        <li>Que se reconocen la personalidad con la que comparecen a la celebración del presente contrato, por lo que otorgan las siguientes:</li>
+                    </ul>
+                    <h2 class="bit-1">C  L  Á  U  S  U  L  A  S</h2>
+                    <p class="bit"><strong>PRIMERA.- OBJETO.-</strong> Por virtud del presente contrato, las partes se obligan a llevar a cabo, conforme a los lineamientos y disposiciones que se indican, el Contrato de Prestación de Servicios Profesionales por virtud del cual <strong>“EL PROFESIONISTA”</strong> se obligará a prestar a <strong>“EL CLIENTE”</strong> la asistencia y los servicios profesionales de asesoría, representación jurídica y el patrocinio legal para la interposición y tramitación del Juicio de Amparo, en todas sus instancias, en contra de las disposiciones legales (normas generales, leyes, decretos, acuerdos, etc) y Autoridades Federales y/o Locales, que atentan contra sus intereses, garantías y derechos fundamentales relativos al libre ejercicio de su profesión. Así como para su representación y defensa ante instancias y Cortes Internacionales, en caso de ser necesario, por la posible vulneración a esos derechos fundamentales por parte de cualquier Autoridad del Estado Mexicano.</p>
+                    <p class="bit"><strong>SEGUNDA.- LINEAMIENTOS Y DISPOSICIONES.-</strong> Las partes se obligan a seguir y  establecer en el Contrato de Prestación de Servicios Profesionales definitivo, esencialmente, los siguientes lineamientos y disposiciones:</p>
+                    <ul class="bit-1">
+                        <li>Tendrá por objeto la prestación de los servicios profesionales de asesoría, representación jurídica y patrocinio legal a cargo de <strong>“EL PROFESIONISTA”</strong> y a favor de <strong>“EL CLIENTE”</strong>, para la interposición y tramitación del Juicio de Amparo, en todas sus instancias, en contra de las disposiciones legales (normas generales, leyes, decretos, acuerdos, etc) y Autoridades Federales y/o Locales, que atentan contra sus intereses, garantías y derechos fundamentales relativos al libre ejercicio de su profesión. Así como para su representación y defensa ante instancias y Cortes Internacionales, en caso de ser necesario, por la posible vulneración a esos derechos fundamentales por parte de cualquier Autoridad del Estado Mexicano.</li>
+                        <li>Que el importe de los honorarios profesionales de <strong>“EL PROFESIONISTA”</strong> asciende a la suma $45,000.00 (cuarenta y cinco mil pesos 00/100 M.N.), sin embargo, se aplicará un descuento del 50% (cincuenta por ciento).</li>
+                        <li>Que <strong>“EL PROFESIONISTA”</strong> correrá con los gastos de tramitación judicial correspondientes.</li>
+                        <li>Que <strong>“EL CLIENTE”</strong> deberá pagar la cantidad de $13,000.00 (trece mil doscientos cincuenta pesos 00/100 M.N.) a la firma del Contrato definitivo de Prestación de Servicios Profesionales, y la cantidad restante, esto es, la suma de $9,500.00 (nueve mil quinientos pesos 00/100 M.N.), a la conclusión del juicio.</li>
+                        <li>Que dentro del importe de los honorarios se encuentra comprendida la cuota de afiliación al <strong>CONSEJO MEXICANO DE LA DEFENSA MÉDICO LEGAL</strong>.</li>
+                        <li>Que con el objeto de defender los intereses de “EL CLIENTE” ante las autoridades judiciales, administrativas, nacionales e internacionales que la defensa requiera, se otorgará a <strong>“EL PROFESIONISTA”</strong> un mandato judicial, especial e irrevocable.</li>
+                        <li>Que <strong>“EL PROFESIONISTA”</strong> se obliga a prestar los servicios profesionales convenidos, poniendo todos sus conocimientos científicos, personal calificado y recursos técnicos, en salvaguarda y protección de los intereses y derechos de <strong>“EL CLIENTE”</strong>.</li>
+                        <li>Que <strong>“EL PROFESIONISTA”</strong> quedará obligado a brindar y rendir toda la información que “EL CLIENTE” le requiera con respecto a la tramitación del o los juicios entablados, así como a brindar la asesoría legal que con respecto a los mismos sea requerida sin costo adicional alguno.</li>
+                        <li>Que <strong>“EL PROFESIONISTA”</strong> deberá guardar secreto profesional sobre el asunto o los asuntos que se le confíen respondiendo por daños y perjuicios en caso de violación según lo establece el artículo 36 de la Ley de Profesiones.</li>
+                    </ul>
+                    <p class="bit-1"><strong>TERCERA.-  PLAZO DE EJECUCIÓN.- “EL CLIENTE”</strong> se obliga a celebrar el Contrato de Prestación de Servicios Profesionales dentro de los 5 días hábiles posteriores a la firma y/o aceptación de este documento.</p>
+                    <p class="bit-1"><strong>CUARTA.- PENA CONVENCIONAL.-</strong> Para el caso no llevarse a cabo la firma del Contrato Definitivo de Prestación de Servicios Profesionales, por causas imputables a “EL CLIENTE”, este se obliga a pagar a <strong>“EL PROFESIONISTA”</strong>, por concepto de pena convencional, la cantidad de $20,000.00 (veinte mil pesos 00/100 M.N.).</p>
+                    <p class="bit-1">Sin perjuicio de lo anterior, las partes pueden convenir, de manera voluntaria y sin responsabilidad alguna, la terminación anticipada y/o rescisión del presente contrato.</p>
+                    <p class="bit-1"><strong>QUINTA.- LEGALIDAD.-</strong> Las partes están plenamente enteradas de los alcances que representa la celebración del presente acto jurídico, en el que no ha existido dolo, error, mala fe, violencia, lesión, ni cualquier otro vicio de la voluntad que afecte o restringa su validez, por lo que se someten de manera irrestricta a la literalidad de las cláusulas pactadas y, en su defecto, a los efectos que se deduzcan de su interpretación integral, y en lo no previsto, se sujetan a las disposiciones del Código Civil para el Distrito Federal.</p>
+                    <p class="bit-1"><strong>SEXTA.- JURISDICCIÓN Y COMPETENCIA.-</strong> Para la interpretación, cumplimiento y ejecución del presente contrato, las partes se someten expresamente a la competencia de los Juzgados Competentes del Tribunal Superior de Justicia del Distrito Federal, renunciando a cualquier otra jurisdicción que pudiera corresponderles en razón de su domicilio presente o futuro.</p>
+                    <p class="bit-1">Enteradas de su contenido y alcance lo firman a los _______ días del mes de abril del año 2015.</p>
+                    <p class="bit-2 last"><strong>“EL CLIENTE”</strong><br /><br /> <br />______________________________________</p>
+                    <p class="bit-2 last"><strong>“EL PROFESIONISTA”</strong><br /><br /><br /><br />CONSEJO MEXICANO DE LA DEFENSA MÉDICO LEGAL, Representada por el Maestro en Amparo Miguel Arroyo Ramírez.</p>
+            </section>
+@stop
