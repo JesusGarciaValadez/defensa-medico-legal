e0c7399e9842b99ec400616ac3a05f00883e6355
alfabrad <zerovamp@hotmail.com>
2015-04-12 07:31:08 +0000

    Adding section error with markup and styles

---
+++ resources/views/error.blade.php
@@ -0,0 +1,13 @@
+@extends( 'app' )
+
+@section( 'title', '¡Error!' )
+
+@section( 'section-class', 'error' )
+
+@section( 'content' )
+            <section class="internal clearfix content bit-1">
+                <article class="clearfix bit-1">
+                    <h1 class="bit-1">¡Lo sentimos!</h1>
+                    <h2 class="bit-1">La información no envió correctamente envía tus datos de contacto al correo <a href="mailto:contactodefensamedicolegal@gmail.com" target="_blank" title="contactodefensamedicolegal@gmail.com">contactodefensamedicolegal@gmail.com</a> o habla a los teléfonos 52944965, fax 55899147 ext. 11 para ponernos en contacto contigo.</h2>
+            </section>
+@stop
